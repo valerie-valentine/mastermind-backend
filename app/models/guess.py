d41e322b053c63bf4bc9b7ad9719b3f428ee74f0
Valerie Valentin <valerie.valentine93@gmail.com>
2024-12-12 13:35:46 -0800

    removed old code & comments, now just includes refactored code

--- app/models/guess.py
+++ app/models/guess.py
@@ -1,34 +1,3 @@
-# from app import db
-
-
-# class Guess(db.Model):
-#     guess_id = db.Column(db.Integer, primary_key=True)
-#     guess = db.Column(db.String, nullable=False)
-#     correct_num = db.Column(db.Integer)
-#     correct_loc = db.Column(db.Integer)
-#     game_id = db.Column(db.Integer, db.ForeignKey(
-#         "game.game_id"), nullable=False)
-#     game = db.relationship("Game", back_populates="guesses")
-
-#     def to_dict(self):
-#         guess_dict = dict(
-#             guess_id=self.guess_id,
-#             guess=self.guess,
-#             correct_num=self.correct_num,
-#             correct_loc=self.correct_loc,
-#             game_id=self.game_id,
-#         )
-
-#         print(guess_dict)
-
-#         return guess_dict
-
-#     @classmethod
-#     def from_dict(cls, guess_data):
-#         new_guess = cls(guess=guess_data["guess"])
-
-#         return new_guess
-
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 from typing import TYPE_CHECKING
--- app/routes/client_routes.py
+++ app/routes/client_routes.py
@@ -8,25 +8,6 @@ from ..db import db, bcrypt
 bp = Blueprint("clients", __name__, url_prefix="/clients")
 
 
-# @bp.route("", methods=["POST"])
-# def create_client():
-#     request_body = request.get_json()
-#     client_data = validate_client(request_body)
-#     hashed_password = bcrypt.generate_password_hash(
-#         client_data["password"]).decode('utf-8')
-
-#     try:
-#         client = Client.from_dict(client_data)
-#         client.password = hashed_password
-#         db.session.add(client)
-#         db.session.commit()
-
-#         return make_response({"client": client.to_dict()}, 201)
-
-#     except KeyError:
-#         abort(make_response(
-#             {'details': f'Failed to create client. Please check your input data'}, 400))
-
 @bp.route("", methods=["POST"])
 def create_client():
     request_body = request.get_json()
