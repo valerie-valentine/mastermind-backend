c3284faee989d46cf5b41a6cd381eab90e9d708b
Valerie Valentin <valerie.valentine93@gmail.com>
2024-12-13 22:01:22 -0800

    Added error-handling to API method

--- app/helpers/random_utils.py
+++ app/helpers/random_utils.py
@@ -1,12 +1,19 @@
 import requests
 
 
-def random_number(digits, num_min, num_max):
+def random_number_api(digits):
     url = f'https://www.random.org/integers/?num={
-        digits}&min={num_min}&max={num_max}&col=1&base=10&format=plain&rnd=new'
+        digits}&min=0&max=9&col=1&base=10&format=plain&rnd=new'
 
-    response = requests.get(url)
-
-    # result digits are new line separated, join them to get a single string
-    random_number = "".join(response.text.split())
-    return random_number
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            random_number = "".join(response.text.split())
+            return random_number
+        else:
+            # Returns 503 for invalid inputs, errors or service unavailable
+            print(f"Failed to connect to random generator API status code:{
+                response.status_code}")
+    # Handles extra cases like connection timeouts/ DNS issues
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while connecting to the API: {e}")
