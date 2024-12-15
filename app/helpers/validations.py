1625a39d5e15f6bf2f4d5513d41746dcfbca2d99
Valerie Valentin <valerie.valentine93@gmail.com>
2024-12-14 18:49:00 -0800

    fixing linting issues

--- app/helpers/validations.py
+++ app/helpers/validations.py
@@ -12,9 +12,7 @@ def validate_model_by_id(cls, model_id):
         response = {"details": f"{cls.__name__} {model_id} invalid"}
         abort(make_response(response, 400))
 
-    # Dynamically finds the primary key column for the model
     primary_key_column = inspect(cls).primary_key[0].name
-
     query = db.select(cls).where(getattr(cls, primary_key_column) == model_id)
     model = db.session.scalar(query)
 
@@ -31,7 +29,7 @@ def ensure_valid_guess_data(game_data, guess):
               guess} invalid. Lives have been exceeded. No more guesses allowed."}, 400))
     if not isinstance(guess, str) or not guess.isdigit():
         abort(make_response({"details": f"Guess: {
-              guess} invalid. Guess must be an numerical value of type string"}, 400))
+              guess} invalid. Guess must be a numerical value of type string"}, 400))
     if len(guess) != game_data.difficulty_level:
         abort(make_response({"details": f"Guess: {guess} invalid. Guess must be {
               game_data.difficulty_level} digits long"}, 400))
@@ -45,7 +43,7 @@ def ensure_valid_guess_data(game_data, guess):
     for saved_guess in game_data.guesses:
         if guess == saved_guess.guess:
             abort(make_response({"details": f"Guess: {
-                guess} invalid. Guess has been played previously"}, 400))
+                  guess} invalid. Guess has been played previously"}, 400))
 
     return guess
 
@@ -54,19 +52,19 @@ def ensure_valid_game_data(request_data):
     if "lives" in request_data:
         if not isinstance(request_data["lives"], int) or int(request_data["lives"]) > 20 or int(request_data["lives"]) < 3:
             abort(make_response(
-                {"details": f"Invalid Choice: Please enter a numerical value for lives between 3 and 20"}, 400))
+                {"details": "Invalid Choice: Please enter a numerical value for lives between 3 and 20"}, 400))
     if "num_min" in request_data:
         if not isinstance(request_data["num_min"], int) or request_data["num_min"] < 0:
             abort(make_response(
-                {"details": f"Invalid Choice: Please enter a numerical value for num_min greater than or equal to 0 and less than 9"}, 400))
+                {"details": "Invalid Choice: Please enter a numerical value for num_min greater than or equal to 0 and less than 9"}, 400))
     if "num_max" in request_data:
         if not isinstance(request_data["num_max"], int) or int(request_data["num_max"]) >= 10 or int(request_data["num_min"]) >= int(request_data["num_max"]):
             abort(make_response(
-                {"details": f"Invalid choice: Please enter a numerical value less than equal to 9 and larger than num_min: {request_data["num_min"]} "}, 400))
+                {"details": f"Invalid choice: Please enter a numerical value less than equal to 9 and larger than num_min: {request_data['num_min']} "}, 400))
     if "difficulty_level" in request_data:
         if not request_data["difficulty_level"] in [4, 6, 8]:
             abort(make_response(
-                {"details": f"Invalid choice: Please enter a valid level: easy, medium, hard"}, 400))
+                {"details": "Invalid choice: Please enter a valid level: easy, medium, hard"}, 400))
 
     return request_data
 
@@ -100,7 +98,7 @@ def ensure_valid_client_login(request_data):
             {'details': 'Failed login. Email and password are required.'}, 400))
     if not isinstance(email, str) or not isinstance(password, str):
         abort(make_response(
-            {'details': 'Falied login. Please enter valid email or password'}, 400))
+            {'details': 'Failed login. Please enter valid email or password'}, 400))
 
     client = Client.query.filter_by(email=email).first()
     if not client or not bcrypt.check_password_hash(client.password, password):
